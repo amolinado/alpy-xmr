398ab13ead7244c7f2db5ff77c714d681fdce768
amolinado <31352466+amolinado@users.noreply.github.com>
2017-09-25 16:14:20 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,11 @@
 ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa && \
 ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa && \
 ssh-keygen -f /etc/ssh/ssh_host_ecdsa_key -N '' -t ecdsa && \
-ssh-keygen -f /etc/ssh/ssh_host_ed25519_key -N '' -t ed25519 && \
-/usr/sbin/sshd 
+ssh-keygen -f /etc/ssh/ssh_host_ed25519_key -N '' -t ed25519 
+#/usr/sbin/sshd 
 
 # Wetty
-node wetty/app.js -p 3000 --sshport ${SSH_PORT:-22} &
+#node wetty/app.js -p 3000 --sshport ${SSH_PORT:-22} &
 
 # Default exec
 EXEC=${EXEC:-xmrig --help}
